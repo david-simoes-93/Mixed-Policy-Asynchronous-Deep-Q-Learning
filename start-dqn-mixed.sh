d4b69e248832cf707b8874bbf48468d44dd3ddc5
bluemoon93 <david.simoes@ua.pt>
2017-09-06 11:53:57 +0100

    Fixed script

--- start-dqn-mixed.sh
+++ start-dqn-mixed.sh
@@ -8,16 +8,16 @@ sleep 5
 
 for j in `seq 1 6`
 do
-        for i in `seq 0 $count`
-        do
-            python3 asyncdqn/DQN-Distributed.py --slaves_per_url="$max" --urls=localhost --task_index="$i" --alg="$j" &
-            echo python3 asyncdqn/DQN-Distributed.py --task_max="$max" --task_index="$i"
-            sleep 5
-        done
+    for i in `seq 0 $count`
+    do
+        python3 asyncdqn/DQN-Distributed.py --slaves_per_url="$max" --urls=localhost --task_index="$i" --alg="$j" &
+        echo python3 asyncdqn/DQN-Distributed.py --task_max="$max" --task_index="$i"
+        sleep 5
+    done
 
-        wait
+    wait
 
-        rm -rf model_dist train_* frames
+    rm -rf model_dist train_* frames
 
-        sleep 100
+    sleep 100
 done
