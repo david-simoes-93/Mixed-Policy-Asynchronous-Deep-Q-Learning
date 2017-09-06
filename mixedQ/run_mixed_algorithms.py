22b1adc5663cff56be365586aa3426a2f03d7fdd
bluemoon93 <david.simoes@ua.pt>
2017-09-06 11:59:24 +0100

    fixed configs

--- mixedQ/run_mixed_algorithms.py
+++ mixedQ/run_mixed_algorithms.py
@@ -21,14 +21,9 @@ def get_action(prob, s):
         action_prob -= prob[s][i]
     return -1
 
-
-#mpl.grid(True)
-#axes = mpl.gca()
-#axes.set_xlim([xmin,xmax])
-#axes.set_ylim([0,1])
 mpl.show()
 
-for game in [5]: #range(4): #todo
+for game in range(6): 
 
     number_of_agents = 2
     number_of_states = 1
@@ -81,7 +76,7 @@ for game in [5]: #range(4): #todo
               [-1, 1, 2, 3],
               [-1, 3, 1, 2]]  # rock paper scissors
 
-    for algorithm in [1]: #range(5): #todo
+    for algorithm in range(6): #todo
         print("Alg: ",algorithm)
 
         plot_prob1=[]
@@ -104,22 +99,10 @@ for game in [5]: #range(4): #todo
         C2 = [0]*number_of_states
 
         Prob1 = np.full((number_of_states,number_of_actions), 1/number_of_actions).tolist()
-        #Prob1[0][0]= random.random()
-        #Prob1[0][0]=0.5
-        #Prob1[0][1] = 1 - Prob1[0][0] / 2
-        #Prob1[0][2] = 1 - Prob1[0][0] / 2
-        #if number_of_actions>2:
-        #    Prob1[0][2] = 0
         Prob2 = np.full((number_of_states,number_of_actions), 1/number_of_actions).tolist()
-        #Prob2[0][0]= random.random()
-        #Prob2[0][0]=0.5
-        #Prob2[0][1]=1-Prob2[0][0]
-        #if number_of_actions>2:
-        #    Prob2[0][2] = 0
 
         Prob_1 = np.zeros((number_of_states,number_of_actions)).tolist()
         Prob_2 = np.zeros((number_of_states,number_of_actions)).tolist()
-        #Prob_1 = list(Prob1)
         Prob_1[0][0]=Prob1[0][0]
         Prob_1[0][1]=Prob1[0][1]
         Prob_2[0][0]=Prob2[0][0]
@@ -130,18 +113,13 @@ for game in [5]: #range(4): #todo
         if number_of_actions > 3:
             Prob_1[0][3] = Prob1[0][3]
             Prob_2[0][3] = Prob2[0][3]
-        #Prob_2 = list(Prob2)
         its = []
         for iteration in range(5000000):
             action1 = get_action(Prob1, 0)
             action2 = get_action(Prob2, 0)
-            #action1 = int(random.random()*number_of_actions) if random.random()<exploration_rate else (0 if Q1[0][0]>Q1[0][1] else 1)
-            #action2 = int(random.random()*number_of_actions) if random.random()<exploration_rate else (0 if Q2[0][0]>Q2[0][1] else 1)
+            
             reward1 = R1[action1][action2]
             reward2 = R2[action1][action2]
-
-            #print(0, 0, action1, reward1, Q1, Prob1, Prob_1, C1)
-            #print(0, 0, action1, reward1, Q1, Prob1, Prob_1, C1)
 
             if algorithm==0:        # Wolf-PHC
                 learning_rate = 0.01
@@ -187,16 +165,6 @@ for game in [5]: #range(4): #todo
                 Prob2[0][0] = Q2[0][0]
                 Prob2[0][1] = Q2[0][1]
                 projection(Prob2[0], 0)
-            """elif algorithm==6:      # WPL
-                learning_rate = 0.01
-                eta = max(0.000001,(learning_rate)/(100+iteration/2000))
-                #eta = learning_rate/100.0
-
-                wpl3(0, 0, action1, reward1, Q1, Prob1, learning_rate, future_rewards_importance, exploration_rate, eta)
-                wpl3(0, 0, action2, reward2, Q2, Prob2, learning_rate, future_rewards_importance, exploration_rate, eta)
-            """
-            #Prob2[0][0] = 0.2
-            #Prob2[0][1] = 1 - Prob2[0][0]
 
             if iteration % 1000 == 0:
                 #print(iteration)
@@ -211,8 +179,6 @@ for game in [5]: #range(4): #todo
                     plot_prob4.append(Prob1[0][3])
                 plot_opp_prob1.append(Prob2[0][0])
                 plot_opp_prob2.append(Prob2[0][1])
-                #plot_q0.append(Q1[0][0])
-                #plot_q1.append(Q1[0][1])
 
                 mpl.clf()
                 mpl.plot(its, plot_prob1, 'r')
@@ -220,11 +186,7 @@ for game in [5]: #range(4): #todo
                 if number_of_actions>3:
                     mpl.plot(its, plot_prob3, 'y')
                     mpl.plot(its, plot_prob4, 'y--')
-                #mpl.plot(its, plot_opp_prob1, 'g')
-                #mpl.plot(its, plot_opp_prob2, 'g--')
-
-                #mpl.plot(its, plot_q0)
-                #mpl.plot(its, plot_q1)
+                
                 mpl.pause(0.0001)
 
             if (iteration) % 10000 == 0:
@@ -235,10 +197,7 @@ for game in [5]: #range(4): #todo
                       '['+' '.join('{0:.{1}f}'.format(k, 3) for k in Prob2[0])+']',
                       '['+' '.join('{0:.{1}f}'.format(k, 3) for k in Prob_2[0])+']')
                 print()
-        #input("")
-
-
+    
         with open("MaQL.txt", "a") as myfile:
-            #print(str(algorithm), str(plot_prob1))
             myfile.write(str(algorithm)+" "+str(log_p1)+" "+str(log_p2)+"\n")
 
