f84bf1f37f432b98758e91a2fc86ba4d84307248
bluemoon93 <david.simoes@ua.pt>
2017-09-19 15:38:01 +0100

    new version

--- simulator/GymRPS.py
+++ simulator/GymRPS.py
@@ -6,22 +6,60 @@ from simulator.Simulator2 import PursuitSim, PREDATOR
 
 
 class GymRPS(object):
-    def __init__(self, number_of_predators=1, number_of_prey=1, numberOfActions=2):
-        self.numberOfActions = numberOfActions
-        self.moves = ["HEADS", "TAILS"]
-
-        #self.numberOfActions = 3
-        #self.moves = ["ROCK", "PAPER", "SCISSORS"]
+    def __init__(self, game="matching_pennies"):
+        if game=="matching_pennies":
+            self.numberOfActions = 2
+            self.moves = ["HEADS", "TAILS"]
+            self.R1 = [[1, -1],
+                       [-1, 1]]
+            self.R2 = [[-1, 1],
+                       [1, -1]]
+        elif game=="biased":
+            self.numberOfActions = 2
+            self.moves = ["Action 0", "Action 1"]
+            self.R1 = [[1.00, 1.75],
+                       [1.25, 1.00]]
+            self.R2 = [[1.75, 1.00],
+                       [1.00, 1.25]]
+        elif game=="tricky":
+            self.numberOfActions = 2
+            self.moves = ["Action 0", "Action 1"]
+            self.R1 = [[0, 3],
+                       [1, 2]]
+            self.R2 = [[3, 2],
+                       [0, 1]]
+        elif game=="rps":
+            self.numberOfActions = 3
+            self.moves = ["ROCK", "PAPER", "SCISSORS"]
+            self.R1 = [[0, -1, 1],
+                  [1, 0, -1],
+                  [-1, 1, 0]]
+            self.R2 = [[0, 1, -1],
+                  [-1, 0, 1],
+                  [1, -1, 0]]
+        elif game=="nrps":
+            self.numberOfActions = 4
+            self.moves = ["NULL", "ROCK", "PAPER", "SCISSORS"]
+            self.R1 = [[-1, -1, -1, -1],
+                       [-1, 2, 1, 3],
+                       [-1, 3, 2, 1],
+                       [-1, 1, 3, 2]]
+            self.R2 = [[-1, -1, -1, -1],
+                       [-1, 2, 3, 1],
+                       [-1, 1, 2, 3],
+                       [-1, 3, 1, 2]]
+        else:
+            print("Unknown game: ", game)
+            exit()
 
         self.debug = False
 
-        self.number_of_predators = number_of_predators  # number of agents
-        self.number_of_prey = number_of_prey
+        self.number_of_predators = 1  # number of agents
+        self.number_of_prey = 1
+        self.input_size = 1
 
-        self.input_size = 1  # *self.numberOfMapCells
-
-        self._screen = np.zeros([number_of_predators, self.input_size])
-        self._screen_prey = np.zeros([number_of_prey, self.input_size])
+        self._screen = np.zeros([1, self.input_size])
+        self._screen_prey = np.zeros([1, self.input_size])
 
         self.reward = 0  # current processed reward
         self.reward_prey = 0
@@ -29,22 +67,8 @@ class GymRPS(object):
         self.terminal = False  # whether simulation is on-going
         self.display = False
 
-        self.R1=[[1.00, 1.75],
-            [1.25, 1.00]] # biased
-        self.R2=[[1.75, 1.00],
-            [1.00, 1.25]]
-
-        self.R1 = [[-1, -1, -1, -1],
-                   [-1, 2, 1, 3],
-                   [-1, 3, 2, 1],
-                   [-1, 1, 3, 2]]  # rock paper scissors
-        self.R2 = [[-1, -1, -1, -1],
-                   [-1, 2, 3, 1],
-                   [-1, 1, 2, 3],
-                   [-1, 3, 1, 2]]  # rock paper scissors
-
-        self.actions = np.zeros(number_of_predators)
-        self.actions_prey = np.zeros(number_of_predators)
+        self.actions = np.zeros(1)
+        self.actions_prey = np.zeros(1)
 
     def new_episode(self):
         return self._screen, self._screen_prey, 0, 0, [0] * self.number_of_predators, [0] * self.number_of_prey, False
@@ -69,29 +93,13 @@ class GymRPS(object):
 
     @property
     def state(self):
-        return self._screen, self.reward, self.terminal
+        return [0], [0], self.reward, self.reward_prey
 
     def birdseye_observe(self):
         return [0]
 
     def predator_observe(self):
-        if self.numberOfActions == 3:
-            if (self.actions[0] == 0 and self.actions_prey[0] == 1) or \
-               (self.actions[0] == 1 and self.actions_prey[0] == 2) or \
-               (self.actions[0] == 2 and self.actions_prey[0] == 3):
-                self.reward = -1
-            elif (self.actions[0] == 0 and self.actions_prey[0] == 2) or \
-                 (self.actions[0] == 1 and self.actions_prey[0] == 0) or \
-                 (self.actions[0] == 2 and self.actions_prey[0] == 1):
-                self.reward = 1
-            else:
-                self.reward = 0
-        else:
-            if self.actions[0] != self.actions_prey[0]:
-                self.reward = 1
-            else:
-                self.reward = -1
-            self.reward = self.R1[self.actions[0]][self.actions_prey[0]]
+        self.reward = self.R1[self.actions[0]][self.actions_prey[0]]
 
         return self._screen, self.reward, self.terminal
 
@@ -99,26 +107,21 @@ class GymRPS(object):
         self.actions = actions
         self.terminal = True
 
+    def observe(self):
+        self.prey_observe()
+        self.predator_observe()
+        return self.state
+
     def prey_observe(self):
-        if self.numberOfActions == 3:
-            if (self.actions_prey[0] == 0 and self.actions[0] == 1) or \
-               (self.actions_prey[0] == 1 and self.actions[0] == 2) or \
-               (self.actions_prey[0] == 2 and self.actions[0] == 3):
-                self.reward_prey = -1
-            elif (self.actions_prey[0] == 0 and self.actions[0] == 2) or \
-                 (self.actions_prey[0] == 1 and self.actions[0] == 0) or \
-                 (self.actions_prey[0] == 2 and self.actions[0] == 1):
-                self.reward_prey = 1
-            else:
-                self.reward_prey = 0
-        else:
-            if self.actions[0] == self.actions_prey[0]:
-                self.reward_prey = 1
-            else:
-                self.reward_prey = -1
-            self.reward_prey = self.R2[self.actions[0]][self.actions_prey[0]]
+        self.reward_prey = self.R2[self.actions[0]][self.actions_prey[0]]
 
         return self._screen_prey, self.reward_prey, self.terminal_prey, [self.terminal]*self.number_of_prey
+
+    def act(self, actions):
+        self.actions = [actions[0]]
+        self.actions_prey = [actions[1]]
+        self.terminal = True
+        self.terminal_prey = True
 
     def prey_act(self, actions):
         self.actions_prey = actions
